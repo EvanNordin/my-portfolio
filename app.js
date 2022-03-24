ec8021fa19662f8cb139a4ab2ebd88e13349158a
Evan Nordin <evan.nordin@cwu.edu>
2022-03-24 13:52:28 -0700

    Added section selection to site
    Added "active" class styling to buttons
    Updated "active" section class lists for content selection

---
+++ app.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+const PageTransitions = () => {
+    //Manpiulating active buttons
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', (event) => {
+            let currentBtn = document.querySelector('.active-btn');
+            let selectedBtn = event.target;
+            currentBtn.className = currentBtn.className.replace('active-btn', '');
+            selectedBtn.className += ' active-btn';
+        })
+    }
+
+    //selecting the section to be displayed
+    allSections.addEventListener('click', (event) => {
+        const id = event.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach(btn => {
+                btn.classList.remove('active');
+            });
+            event.target.classList.add('active');
+
+            //hide the other sections
+            sections.forEach(section => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
