8c88b73db2cf98720e2fd7246e0f4e5ffaefc5cd
Evan Nordin <evan.nordin@cwu.edu>
2022-03-28 12:13:40 -0700

    finished overall layout and styling

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ const PageTransitions = () => {
             element.classList.add('active');
         }
     })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', (event) => {
+        let element = document.body;
+        element.classList.toggle('light-theme');
+    })
 }
 
 PageTransitions();
